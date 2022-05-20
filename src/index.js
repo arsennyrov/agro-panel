215f2655a080ad6792f304f7c8439def72bfed53
arsennyrov <arsenyrov@mail.ru>
2022-05-20 09:58:04 +0300

    Добавил шрифт TT Norms

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import './fonts/TT Norms/TTNorms-Black.otf';
 
 import './index.css';
 import App from './App';
