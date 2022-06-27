4a5e958899c727de525cf6d7e5a56a8b4b7c2490
emustafin <eldar.mustafin@gmail.com>
2022-06-27 17:25:11 +0300

    Код для запуска на Fiori, раскомментить при build и закомментить все, что дальше

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import './index.css';
 import App from './App';
 import { store } from './stores';
 
+// window.ReactDOM = ReactDOM;
+// window.React = React;
+// window.Provider = Provider;
+// window.App = App;
+// window.store = store;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
