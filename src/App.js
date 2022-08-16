300a9606155661012297c2cc2713d5d559411e85
arsennyrov <arsenyrov@mail.ru>
2022-08-16 16:47:32 +0300

    В App.js заменил BrowserRouter на HashRouter

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
 import Page31 from './pages/Page31';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import { HashRouter, Routes , Route } from 'react-router-dom';
 
 
 function App() {
   return (
 
       <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Page1 />} />
           <Route path="/panels/1" element={<Page1 />} />
           <Route path="/panels/2" element={<Page2 />} />
           <Route path="/panels/3" element={<Page3 />} />
           <Route path="/panels/:id" element={<Page31 />} />
-
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       </div>
     
   );
